ecffff6f6d84b09713f0b3bb5b8479aade400615
pasindu <pasindu@codegen.net>
2019-06-18 14:13:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
+ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+
 
 import { AppComponent } from './app.component';
 
