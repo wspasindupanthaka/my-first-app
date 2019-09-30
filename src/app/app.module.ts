9fd19769deea55a6c6a082074eaaf2f820159d43
pasindu <pasindu@codegen.net>
2019-09-30 18:45:19 +0530

    string interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
  import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ServerComponent } from './server/server.component';
 
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ErrorAlertComponent } from './error-alert/error-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    ErrorAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  selector:`app-servers`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() =>{
+      this.allowNewServer=true;
+    },7000)
+   }
+
+  ngOnInit() {
+  }
+
+}
